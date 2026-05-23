ef84104fe0f8172da3e54eca3a66017832aca4f6
Wendyam BONKOUNGOU <mariabon205@gmail.com>
2026-05-23 16:17:34 +0000

    Add main function for Fasoagrifinance system

---
+++ fasoagrifinance.py
@@ -0,0 +1,9 @@
+def main():
+    print("=" * 60)
+    print("        WELCOME TO FASOAGRIFINANCE SYSTEM")
+    print("        Burkina Institute of Technology")
+    print("=" * 60)
+ 
+ 
+if __name__ == "__main__":
+    main()
