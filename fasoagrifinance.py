670d44bad7ae19f0129a249e122b5f78dc490a58
Alima06 <simpore302@gmail.com>
2026-05-24 12:32:18 +0000

    Delete fasoagrifinance.py

--- fasoagrifinance.py
+++
@@ -1,9 +0,0 @@
-def main():
-    print("=" * 60)
-    print("        WELCOME TO FASOAGRIFINANCE SYSTEM")
-    print("        Burkina Institute of Technology")
-    print("=" * 60)
- 
- 
-if __name__ == "__main__":
-    main()
